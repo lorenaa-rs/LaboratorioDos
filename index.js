13c6d0d9dc1531e8b08b9ae79fc3449254e46d93
lorenaa-rs <55335909+lorenaa-rs@users.noreply.github.com>
2021-09-07 13:50:03 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,131 +1,95 @@
+
 const express = require('express')
 const app = express()
-//const port = process.argv[2];
-const port = 3000;
-const cors = require('cors');
-const shellExec = require('shell-exec');
-const shell = require('shelljs');
-const nodemailer = require('nodemailer');
+const path = require("path");
+const port = process.argv[2];
+//const port = 3001;
 const bodyparser = require('body-parser')
-const loadbalance = require('loadbalance')
-var path = require('path')
-var fs = require('fs')
-var morgan = require('morgan')
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
-const axios = require('axios');
-var servers = ["http://localhost:4000/",
-    "http://localhost:4001/",
-    "http://localhost:4002/"]
-var maxErrors = 3;    
-var portNew = 4003;
-var engine = loadbalance.roundRobin(servers)
-var count = 0;
+const multer = require('multer')
+const cors = require('cors');
+var sizeOf = require('image-size');
+const download = require('image-downloader')
+var srvActual = "";
+var Jimp = require("jimp");
+const { default: axios } = require('axios');
+const { LOADIPHLPAPI } = require('dns');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
+
 app.use(express.json())
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(cors())
-app.use(morgan('combined', {stream: accessLogStream}))
-app.use(require('express-status-monitor')());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    loadBalancer(res);
+    console.log("Connection");
+    res.send('http://localhost:' + port + '/')
 })
 
-app.post('/addServer', (req, res) => {    
-    shell.exec(`sh bash.sh ${portNew}`)
-    axios.get("http://localhost:"+portNew+"/")
-        .then(function (response) {            
-            console.log("Servidor agregado: ")
-            console.log('>'+response.data+'<')
-            servers.push("http://localhost:"+portNew+"/")
-            engine = loadbalance.roundRobin(servers)
-            maxErrors = maxErrors + 1
-            res.send("Servidor correctamente agregado"+"http://localhost:"+portNew+"/")
-            console.log("Servers: " + servers)
-            portNew++;
-            var now = new Date();
-            var date = now.getDate()+"/"+(now.getMonth()+1)+"/"+now.getFullYear();
-            var hour = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
-            var report = date+"-"+hour+"-"+servers;
-            shell.exec(`sh write_report.sh ${report}`)
-        })
-        .catch(function (error) {            
-            var txt = "Servidor: " + "http://localhost:"+portNew+"/" + " no disponible";
-            res.send(txt)
-            console.log(txt)
-           
-        })
-    
-})
-
-function loadBalancer(res) {
-    if (count < maxErrors) {
-        var srv = engine.pick();
-        console.log(srv + " ha recibido peticion");
-        axios.get(srv)
-            .then(function (response) {
-                axios.get(srv+"saveServer", {
-                    params: {
-                      name: srv
-                    }
-                  })
-                //console.log("count: " + count)
-                count = 0;
-                res.send(srv)
-                var now = new Date();
-            var date = now.getDate()+"/"+(now.getMonth()+1)+"/"+now.getFullYear();
-            var hour = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
-            var report = date+"-"+hour+"-"+srv;
-            
-                shell.exec(`sh write_report-servidor.sh ${report}`)
-
-            }).catch(function (error) {
-                console.log("Error")
-                loadBalancer(res);
-            })
-
-            .catch(function (error) {
-                var txt = "Servidor: " + srv + " no disponible";
-                console.log(txt)
-                count = count + 1;
-                //console.log("count: " + count)
-                mail(txt);
-                loadBalancer(res);
-            })
-
-    } else {
-        res.send('Error')
-        console.log("Servidores fuera de servicio")
-        count = 0;
+app.post('/loadImg', upload.single('files'), (req, res, next) => {
+    console.log("Connection")
+    const file = req.file
+    if (!file) {
+        const error = new Error('Please upload a file')
+        error.httpStatusCode = 400
+        return next(error)
     }
+    console.log(file.filename)
+    axios.get('https://geek-jokes.sameerkumar.website/api?format=json').then(function(response){
+        editImg('public/' + file.filename, response.data.joke);
+        res.send(file.filename)
+    })
+   
+})
 
-}
+app.get('/saveServer', (req, res) => {
+    console.log(req.param('name'));
+    console.log("el save Server")
+    srvActual = req.param('name')
+   console.log(srvActual)
 
-function mail(data) {
-    
-    var trasnporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'pruebadistribuidos12',
-            pass: 'Distribuidos2021'
+})
+
+app.post('/getImage', (req, res) => {
+    console.log(req.body);
+    console.log( "<img src='" + srvActual + '/' + req.body.fileName + "'" + "/>")
+console.log(srvActual)
+    var divFile = "<div>"
+        + "<img src='" + srvActual + req.body.fileName + "'" + "/>"
+        + "</div>"
+    //res.sendFile(__dirname + '/public/' + req.body.fileName)
+    res.end(divFile);
+})
+
+function editImg(filePath, text) {
+    var wi = ""
+    var he = ""
+    sizeOf(filePath, function (err, dimensions) {
+        if (!err) {
+            wi = dimensions.width;
+            he = dimensions.height;
         }
     });
-    var infoMessage = {
-        from: 'pruebadistribuidos12@gmail.com',
-        to: 'lorena.rioss@uptc.edu.co',
-        subject: 'Falla en el sistema',
-        text: data
-    };
-    trasnporter.sendMail(infoMessage, function (error, info) {
-        if (error) {
-            console.log("error enviando correo");
-            console.log(error);
-        } else {
-            console.log("Correo enviado");
-        }
-    });
+    Jimp.read(filePath)
+        .then(function (image) {
+            loadedImage = image;
+            return Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+        })
+        .then(function (font) {
+            loadedImage.print(font, (wi * 20/ 100), (he *80/100), text)
+                .write(filePath);
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
 }
 
 app.listen(port, () => {
